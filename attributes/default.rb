57ce34c2cbb5c24dea6f446cc8b12080b505d730
Sébastien Grégoire <greg@cakemail.com>
2013-11-20 18:21:28 -0500

    support for multiple framebuffers

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,5 @@
 set[:xvfb][:display] = 99
+
+# multiple display support
+default[:xvfb][:first_display] = 1
+default[:xvfb][:last_display]  = 32
--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
-maintainer       "Maciej Pasternacki"
-maintainer_email "maciej@pasternacki.net"
+maintainer       "Sebastien Gregoire"
+maintainer_email "greg@cakemail.com"
 license          "apachev2"
 description      "Installs/Configures xvfb"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.1"
+version          "0.0.2"
 
 supports "debian"
